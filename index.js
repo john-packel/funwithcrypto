a7d0fb1d7391a3262ac05cf2e22edb78976248fc
John Packel <john.packel@gmail.com>
2017-04-02 14:18:32 -0400

    Trying path.join to debug heroku deploy issue.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 var port = process.env.PORT || 8888;
 
+// app.use(express.static(path.join(__dirname + 'client')));
+// app.use(express.static('client'));
+
 app.get('/', function(req, res) {
   res.sendfile('client/Coindesk parody.html');
 })
+
 app.listen(port, function() {
   console.log("Coinparody server listening for crypto on port: ", port);
 })
