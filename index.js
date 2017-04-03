d3831eb34e70c1b1ea5b3cb3caed489ad84a6eca
John Packel <john.packel@gmail.com>
2017-04-02 22:34:42 -0400

    Got express static serving

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 
 var port = process.env.PORT || 8888;
 
-// app.use(express.static(path.join(__dirname + 'client')));
-// app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'client')));
+// app.use('/static', express.static('./client'));
 
-app.get('/', function(req, res) {
-  // res.sendfile('client/Coindesk parody.html');
-  res.sendfile('./client/index.html');
-})
+// app.get('/', function(req, res) {
+//   // res.sendfile('client/Coindesk parody.html');
+//   res.sendfile('./client/index.html');
+// })
 
 app.listen(port, function() {
   console.log("Coinparody server listening for crypto on port: ", port);
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+var port = process.env.PORT || 8888;
+
+// app.use(express.static(path.join(__dirname + 'client')));
+// app.use(express.static('client'));
+
+app.get('/', function(req, res) {
+  // res.sendfile('client/Coindesk parody.html');
+  res.sendfile('./index.html');
+})
+
+app.listen(port, function() {
+  console.log("Coinparody server listening for crypto on port: ", port);
+})
