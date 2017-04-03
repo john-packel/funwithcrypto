a0e59f42977b7a82a5f2a88bfa0f43c4741b2e5f
John Packel <john.packel@gmail.com>
2017-04-02 23:03:54 -0400

    try force-ssl-heroku

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var forceSsl = require('force-ssl-heroku');
+
+app.use(forceSsl);
 
 var port = process.env.PORT || 8888;
 
 // app.use(express.static(path.join(__dirname + 'client')));
 // app.use(express.static('client'));
+
 
 app.get('/', function(req, res) {
   // res.sendfile('client/Coindesk parody.html');
